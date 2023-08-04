43b867bdc47ae37391a07a77979cf0afd2f509f9
omari <omarkayumba12345@gmail.com>
2023-08-04 10:27:16 +0100

    Course01_REDUX

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Counter from "./features/counter/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="App">
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
